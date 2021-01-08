5fc5011b77f9c8fb371f1f09519eb1180b9efa56
a.anufriev <anufriev.artem.mail@gmail.com>
2021-01-08 12:32:19 +0300

    Added Dockerfile

--- configs/const.go
+++ configs/const.go
@@ -4,7 +4,7 @@ const (
 	ApiPort                = ":5000"
 	ApiUrl                 = "/api"
 	Postgres               = "postgres"
-	DataSourceNamePostgres = "host=localhost dbname=forum sslmode=disable"
+	DataSourceNamePostgres = "host=localhost user=docker password=docker dbname=forum sslmode=disable"
 	RequestID              = "reqID"
 	Limit                  = "limit"
 	Desc                   = "desc"
